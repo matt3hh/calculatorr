3d770d8aed3e3847d51a4fffe8a33fd19bec4364
matt3hh <matildagranstrom92@gmail.com>
2024-11-10 09:28:26 +0200

    Add project files.

---
+++ test/test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+	char operation; 
+	double num1, num2;
+	char repeat;
+
+	do {
+
+		cout << "simple calculator" << endl;
+		cout << "choose an operation (+ or -)";
+		cin >> operation;
+
+		if (operation != '+' && operation != '-') {
+			cout << "invalid operation! Use + or -" << endl;
+			return 1;
+		}
+		cout << "enter the first number: ";
+		cin >> num1;
+		cout << "enter second number: ";
+		cin >> num2;
+
+		if (operation == '+') {
+			cout << "result: " << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+
+		}
+		else if (operation == '-') {
+			cout << "result: " << num1 << " - " << num2 << " = " << (num1 - num2) << endl;
+		}
+		cout << "would you like to perform another calculation? (y/n): ";
+		cin >> repeat;
+	} while (repeat == 'y' || repeat == 'Y');
+
+
+	cout << "thank you for using the calculator, Byeee!" << endl;
+
+
+
+	return 0;
+
+}
+
+
